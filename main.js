8aae8268487493e318d177c332aef18a4fec5e62
Jana <23-101109@students.eui.edu.eg>
2025-12-22 10:55:37 +0200

    starting up header and theme switcher

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ window.addEventListener("load", () => {
 });
 
 // preloader
+
+// theme switcher
+
+document.getElementById("mode-toggle").innerHTML = document.getElementById("mode-toggle").innerHTML;
+document.getElementById("mode-toggle").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+
+// theme switcher
